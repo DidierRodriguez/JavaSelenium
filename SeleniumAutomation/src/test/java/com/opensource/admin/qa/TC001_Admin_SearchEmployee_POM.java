93e8895e1fdcce673f6344a331df4dcd3e53598c
DidierRodriguez <rodriguezdidier3@gmail.com>
2021-05-27 14:57:36 -0500

    Completando POM y agregando metodos para cargar Excel y JSON File

--- SeleniumAutomation/src/test/java/com/opensource/admin/qa/TC001_Admin_SearchEmployee_POM.java
+++ SeleniumAutomation/src/test/java/com/opensource/admin/qa/TC001_Admin_SearchEmployee_POM.java
@@ -1,19 +1,72 @@
 package com.opensource.admin.qa;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.opensource.admin.Login;
+import com.opensource.admin.UserManagment;
+import com.opensource.base.GlobalVariables;
+import com.opensource.base.SeleniumWrapper;
 
 public class TC001_Admin_SearchEmployee_POM {
-  @Test
-  public void f() {
-  }
+	
+	WebDriver driver;
+	SeleniumWrapper seleniumWrapper;
+	Login login;
+	UserManagment userManagment;
+	// Hardcode de datos -nunca hacer esto
+	String username, password;
+
   @BeforeTest
   public void beforeTest() {
+	  
+	  seleniumWrapper=new SeleniumWrapper(driver);
+	  driver = seleniumWrapper.chromeDriverConnection();
+	  login = new Login(driver);
+	  userManagment = new UserManagment(driver);
+	  
+	  //Hardcode de datos -nunca hacer esto
+	  username = "Admin";
+	  password = "admin123";
+	  
   }
 
+  
+  @Test
+  public void TC001_Admin_SearchEmployee_POM_TestScript() {
+	  
+	  // Step 1
+	  seleniumWrapper.launchBrowser(GlobalVariables.QA_URL);
+	  
+	  // Step 2
+	  login.loginOrange(username, password);
+	  
+	  // Step 3
+	  userManagment.validateLogged();
+	  
+	  // Step 4
+	  userManagment.clickAdmin();
+	  
+	  // Step 5 and 6
+	  userManagment.searchUser("Admin", true);
+	  
+	  // Step 7
+	  userManagment.validateFormSearchTable("Admin");
+	  
+	  // Step 8
+	  login.logoutOrange();
+	  
+  }
+  
+  
   @AfterTest
   public void afterTest() {
+	  
+	  // Step 9 
+	  seleniumWrapper.closeBrowser();
+	  
   }
 
 }
